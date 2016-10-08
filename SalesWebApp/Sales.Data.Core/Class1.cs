f093a3f294cbdcbbc9598543e7280bfe16b41e66
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-10-08 14:26:03 +0530

    A

--- SalesWebApp/Sales.Data.Core/Class1.cs
+++ SalesWebApp/Sales.Data.Core/Class1.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Sales.Data.Core
+﻿namespace Sales.Data.Core
 {
     public class Class1
     {
--- SalesWebApp/Sales.ServiceApi/MainCls.cs
+++ SalesWebApp/Sales.ServiceApi/MainCls.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
 
 namespace Sales.ServiceApi
 {
-    public class Program
+    public class MainCls
     {
         public static void Main(string[] args)
         {
