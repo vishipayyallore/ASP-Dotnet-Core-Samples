bfd5783d1972806e037ac697633ca30c64fb1212
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-10-08 16:46:07 +0530

    E

--- SalesWebApp/Sales.WebPortal/wwwroot/_references.js
+++ SalesWebApp/Sales.WebPortal/wwwroot/_references.js
@@ -1,5 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/site.js" />
+/// <reference path="lib/angular/angular.js" />
+/// <reference path="lib/angular-ui-router/release/angular-ui-router.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
