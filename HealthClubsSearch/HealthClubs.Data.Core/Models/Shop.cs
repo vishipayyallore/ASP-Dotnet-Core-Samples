9d40b1fe18e88dd74dc3a01dcb2e3071b5642c27
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-12 20:50:00 +0530

    1

---
+++ HealthClubsSearch/HealthClubs.Data.Core/Models/Shop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HealthClubs.Data.Core.Models
+{
+    public class Shop
+    {
+        public Shop() { }
+
+        #region Properties
+        public Guid Id { get; set; }
+
+        public string BusinessName { get; set; }
+
+        public string BusinessType { get; set; }
+
+        public string Link { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal Rating { get; set; }
+        #endregion
+    }
+}
